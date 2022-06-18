7db7fe930db74cad7a64e15a23d595d3a1f3a135
bond_307 <freelancer4747.com>
2022-06-18 10:10:02 +0600

    Column, Text, Text Style, Icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,113 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(MaterialApp(
-    home: MyApp(),
-  ),
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
-      return Scaffold(
+    return MaterialApp(
+      home: HomeScreen(),
+    );
 
-          appBar: AppBar(
-            title: Text("Flutter Apps"),
-          ),
-
-          body: Center(
-            child: Text("body Text center"),
-          ),
-
-          floatingActionButton: FloatingActionButton(
-            onPressed: (){},
-            child: Text("Click"),
-          )
-
-
-      );
   }
 
+}
+
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  int num = 0;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+        appBar: AppBar(
+          title: Text("Flutter Apps"),
+        ),
+
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Text Style flutter",
+              style: TextStyle(
+                // fontFamily: ,
+                  color: Colors.white,
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                  backgroundColor: Colors.black,
+                  letterSpacing: 5.0
+              ),
+            ),
+            Text(
+              "Text Style flutter2",
+              style: TextStyle(
+                // fontFamily: ,
+                  color: Colors.white,
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                  backgroundColor: Colors.red,
+                  letterSpacing: 5.0
+              ),
+            ),
+            Text(
+              "Text Style flutter3",
+              style: TextStyle(
+                // fontFamily: ,
+                  color: Colors.white,
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                  backgroundColor: Colors.blue,
+                  letterSpacing: 5.0
+              ),
+            ),
+
+            Icon(
+              Icons.add_a_photo_rounded,
+              size: 50.0,
+              color: Colors.amber,
+              shadows: [
+                BoxShadow(
+                  color: Colors.black,
+                  blurRadius: 50.0,
+                  spreadRadius: 20.0,
+                  offset: Offset(6, 5)
+                )
+              ],
+            )
+
+          ],
+        ),
+
+
+
+
+
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            setState((){
+              num++;
+            });
+          },
+          child: Text("Click"),
+        )
+
+
+    );
+  }
 }
 
 
